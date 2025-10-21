280ea2ae7db768d7ce15122ed4869bb8b779a5f5
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2025-10-21 19:54:22 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "output_writer/write_paraview_output.h"
+
+
+#include <iostream>
+
+#include <cstdlib>
+
+
+int main(int argc, char *argv[])
+
+{
+
+  // write 5 output files
+
+  for (int i = 0; i < 5; i++)
+
+  {
+
+    writeParaviewOutput(i);
+
+  }
+
+
+  return EXIT_SUCCESS;
+
+}
