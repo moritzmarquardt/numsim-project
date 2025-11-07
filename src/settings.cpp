fee07932e1b1de98868ae9a02c20dacf1d882f4d
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2025-11-07 16:48:58 +0100

    i think were done

--- src/settings.cpp
+++ src/settings.cpp
@@ -24,12 +24,123 @@ void Settings::loadFromFile(std::string filename){
     std::string line;
     getline(file, line);
 
+    // remove whitespace at beginning of line
+    line.erase(0, line.find_first_not_of(" \t")); 
+
+    // if first character is a '#', skip line
+    if (line[0] == '#')
+      continue; 
+
+    // if line does not contain a '=' sign, skip line
+    if (line.find('=') == std::string::npos)
+      continue; 
+
+    // extract and isolate parameter name
+    std::string parameterName = line.substr(0, line.find('='));
+    if (parameterName.find_first_of(" \t") != std::string::npos)
+    {
+      parameterName.erase(parameterName.find_first_of(" \t"));
+    }
+
+    // extract and isolate value
+    std::string value = line.substr(line.find('=') + 1);
+
+    // remove whitespace at beginning of value
+    value.erase(0, value.find_first_not_of(" \t"));
+    // remove comments at end of value
+    if (value.find_first_of("#") != std::string::npos)
+      value.erase(value.find_first_of("#"));
+    // remove whitespace at end of value
+    value.erase(value.find_last_not_of(" \t") + 1); 
+    
+    // match parameterName and value to given variables
+    if (parameterName == "endTime"){
+      endTime = atof(value.c_str());
+    }
+    else if (parameterName == "physicalSizeX"){ 
+      physicalSize[0] = atof(value.c_str());
+    }
+    else if (parameterName == "physicalSizeY"){
+      physicalSize[1] = atof(value.c_str());
+    }
+    else if (parameterName == "re"){
+      re = atof(value.c_str());
+    }
+    else if (parameterName == "gX"){
+      g[0] = atof(value.c_str());
+    }
+    else if (parameterName == "gY"){
+      g[1] = atof(value.c_str());
+    }
+    else if (parameterName == "dirichletBottomX"){
+      dirichletBcBottom[0] = atof(value.c_str());
+    }
+    else if (parameterName == "dirichletBottomY"){
+      dirichletBcBottom[1] = atof(value.c_str());
+    }
+    else if (parameterName == "dirichletTopX"){
+      dirichletBcTop[0] = atof(value.c_str());
+    }
+    else if (parameterName == "dirichletTopY"){
+      dirichletBcTop[1] = atof(value.c_str());
+    }
+    else if (parameterName == "dirichletLeftX"){
+      dirichletBcLeft[0] = atof(value.c_str());
+    }
+    else if (parameterName == "dirichletLeftY"){
+      dirichletBcLeft[1] = atof(value.c_str());
+    }
+    else if (parameterName == "dirichletRightX"){
+      dirichletBcRight[0] = atof(value.c_str());
+    }
+    else if (parameterName == "dirichletRightY"){
+      dirichletBcRight[1] = atof(value.c_str());
+    }
+    else if (parameterName == "nCellsX"){
+      nCells[0] = atoi(value.c_str());
+    }
+    else if (parameterName == "nCellsY"){
+      nCells[1] = atoi(value.c_str());
+    }
+    else if (parameterName == "useDonorCell"){
+      if(value == "true"){
+        useDonorCell = true;
+      }
+      else if(value == "false"){
+        useDonorCell = false;
+      }
+      else{
+        std::cout << "Unknown value for useDonorCell: " << value << std::endl;
+      }
+    }
+    else if (parameterName == "alpha"){
+      alpha = atof(value.c_str());
+    }
+    else if (parameterName == "tau"){
+      tau = atof(value.c_str());
+    }
+    else if (parameterName == "maximumDt"){
+      maximumDt = atof(value.c_str());
+    }
+    else if (parameterName == "pressureSolver"){
+      pressureSolver = value;
+    }
+    else if (parameterName == "omega"){
+      omega = atof(value.c_str());
+    }
+    else if (parameterName == "epsilon"){
+      epsilon = atof(value.c_str());
+    }
+    else if (parameterName == "maximumNumberOfIterations"){
+      maximumNumberOfIterations = static_cast<int>(atof(value.c_str()));
+    }
+    else{
+      std::cout << "Unknown parameter \"" << parameterName << "\" in line " << lineNo << std::endl;
+    }
+
     // at the end of the file break for loop
     if (file.eof())
       break;
-
-    // print line 
-    std::cout << "line " << lineNo << ": " << line << std::endl;
   }
 }
 
