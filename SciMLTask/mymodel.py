174dd0301aefdfc858fa1fa17bc1b4d3ee0cb4d8
Moritz Marquardt <moritz.g.marquardt@gmail.com>
2026-01-07 15:38:18 +0100

    bdm

--- SciMLTask/mymodel.py
+++ SciMLTask/mymodel.py
@@ -1,90 +1,33 @@
-from typing import OrderedDict
-import torch
+import torch.nn as nn
 
 
-def init_my_model(
-    in_channels: int = 1, out_channels: int = 2, activation_function=torch.nn.ReLU()
-):
-    model = torch.nn.Sequential(
-        OrderedDict(
-            [
-                (
-                    "conv1",
-                    torch.nn.Conv2d(
-                        in_channels=in_channels,
-                        out_channels=16,
-                        kernel_size=7,
-                        padding="same",
-                        stride=1,
-                    ),
-                ),
-                ("relu1", activation_function),
-                (
-                    "conv2",
-                    torch.nn.Conv2d(
-                        in_channels=16,
-                        out_channels=16,
-                        kernel_size=7,
-                        padding="same",
-                        stride=1,
-                    ),
-                ),
-                ("relu2", activation_function),
-                (
-                    "conv3",
-                    torch.nn.Conv2d(
-                        in_channels=16,
-                        out_channels=16,
-                        kernel_size=7,
-                        padding="same",
-                        stride=1,
-                    ),
-                ),
-                ("relu3", activation_function),
-                (
-                    "conv4",
-                    torch.nn.Conv2d(
-                        in_channels=16,
-                        out_channels=16,
-                        kernel_size=7,
-                        padding="same",
-                        stride=1,
-                    ),
-                ),
-                ("relu4", activation_function),
-                (
-                    "conv5",
-                    torch.nn.Conv2d(
-                        in_channels=16,
-                        out_channels=16,
-                        kernel_size=7,
-                        padding="same",
-                        stride=1,
-                    ),
-                ),
-                ("relu5", activation_function),
-                (
-                    "conv6",
-                    torch.nn.Conv2d(
-                        in_channels=16,
-                        out_channels=16,
-                        kernel_size=7,
-                        padding="same",
-                        stride=1,
-                    ),
-                ),
-                ("relu6", activation_function),
-                (
-                    "conv7",
-                    torch.nn.Conv2d(
-                        in_channels=16,
-                        out_channels=out_channels,
-                        kernel_size=7,
-                        padding="same",
-                        stride=1,
-                    ),
-                ),
-            ]
-        )
-    )
-    return model
+class MyModel(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, activation_function: nn.Module):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=7, padding="same", stride=1)
+        self.af1 = activation_function
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=7, padding="same", stride=1)
+        self.af2 = activation_function
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=7, padding="same", stride=1)
+        self.af3 = activation_function
+        self.conv4 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=7, padding="same", stride=1)
+        self.af4 = activation_function
+        self.conv5 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=7, padding="same", stride=1)
+        self.af5 = activation_function
+        self.conv6 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=7, padding="same", stride=1)
+        self.af6 = activation_function
+        self.conv7 = nn.Conv2d(in_channels=16, out_channels=out_channels, kernel_size=7, padding="same", stride=1)
+
+    def forward(self, x):
+        x = self.af1(self.conv1(x))
+        x = self.af2(self.conv2(x))
+        x = self.af3(self.conv3(x))
+        x = self.af4(self.conv4(x))
+        x = self.af5(self.conv5(x))
+        x = self.af6(self.conv6(x))
+        x = self.conv7(x)
+        return x
+
+
+def init_my_model(in_channels=1, out_channels=2, activation_function=nn.SiLU()):
+    return MyModel(in_channels=in_channels, out_channels=out_channels, activation_function=activation_function)
