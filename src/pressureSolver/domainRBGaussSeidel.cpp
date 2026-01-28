2d618ce1be334e5e57909e8b8f53188d07663503
mpvogel <maxvogel344@gmail.com>
2026-01-28 16:23:08 +0100

    set boundaries before communication in pressure solver

--- src/pressureSolver/domainRBGaussSeidel.cpp
+++ src/pressureSolver/domainRBGaussSeidel.cpp
@@ -18,7 +18,6 @@ void DomainRBGaussSeidel::solve() {
 
         std::vector<CellInfo> redCellsInfo = domain_->getRedListFluid();
         int n = redCellsInfo.size();
-        double dx = discretization_->dx();
         for (int idx = 0; idx < n; idx++) {
             CellInfo redCellInfo = redCellsInfo[idx];
             int i = redCellInfo.cellIndexPartition[0];
@@ -32,43 +31,91 @@ void DomainRBGaussSeidel::solve() {
 
             if (redCellInfo.topIsBoundaryFace()) {
                 p_i_jp1 = p_i_j;
-                if (partitioning_->ownPartitionContainsTopBoundary() && j == discretization_->pJEnd()) {
-                    // Dirichlet BC at top boundary
-                    discretization_->p(i, j + 1) = p_i_jp1; // assuming ghost cell already set to BC value
-                }
+                // if (partitioning_->ownPartitionContainsTopBoundary() && j == discretization_->pJEnd()) {
+                //     // Dirichlet BC at top boundary
+                //     discretization_->p(i, j + 1) = p_i_jp1; // assuming ghost cell already set to BC value
+                // }
             }
             if (redCellInfo.bottomIsBoundaryFace()) {
                 p_i_jm1 = p_i_j;
-                if (partitioning_->ownPartitionContainsBottomBoundary() && j == discretization_->pJBegin()) {
-                    // Dirichlet BC at bottom boundary
-                    discretization_->p(i, j - 1) = p_i_jm1; // assuming ghost cell already set to BC value
-                }
+                // if (partitioning_->ownPartitionContainsBottomBoundary() && j == discretization_->pJBegin()) {
+                //     // Dirichlet BC at bottom boundary
+                //     discretization_->p(i, j - 1) = p_i_jm1; // assuming ghost cell already set to BC value
+                // }
             }
             if (redCellInfo.leftIsBoundaryFace()) {
                 p_im1_j = p_i_j;
-                if (partitioning_->ownPartitionContainsLeftBoundary() && i == discretization_->pIBegin()) {
-                    // Dirichlet BC at left boundary
-                    discretization_->p(i - 1, j) = p_im1_j; // assuming ghost cell already set to BC value
-                }
+                // if (partitioning_->ownPartitionContainsLeftBoundary() && i == discretization_->pIBegin()) {
+                //     // Dirichlet BC at left boundary
+                //     discretization_->p(i - 1, j) = p_im1_j; // assuming ghost cell already set to BC value
+                // }
             }
             if (redCellInfo.rightIsBoundaryFace()) {
                 p_ip1_j = p_i_j;
-                if (partitioning_->ownPartitionContainsRightBoundary() && i == discretization_->pIEnd()) {
-                    // Dirichlet BC at right boundary
-                    discretization_->p(i + 1, j) = p_ip1_j; // assuming ghost cell already set to BC value
-                }
+                // if (partitioning_->ownPartitionContainsRightBoundary() && i == discretization_->pIEnd()) {
+                //     // Dirichlet BC at right boundary
+                //     discretization_->p(i + 1, j) = p_ip1_j; // assuming ghost cell already set to BC value
+                // }
             }
 
             discretization_->p(i,j) = lek * ((p_ip1_j + p_im1_j)/dx2 + (p_i_jp1 + p_i_jm1) / dy2 - discretization_->rhs(i,j));
+
+            // // Todo: on partition boundary set Neumann zero BC after udating pressure.
+
+            // if (redCellInfo.topIsBoundaryFace() && partitioning_->ownPartitionContainsTopBoundary() && j == discretization_->pJEnd()) {
+            //     // Dirichlet BC at top boundary
+            //     discretization_->p(i, j + 1) = discretization_->p(i, j); // assuming ghost cell already set to BC value
+            // }
+            // if (redCellInfo.bottomIsBoundaryFace() && partitioning_->ownPartitionContainsBottomBoundary() && j == discretization_->pJBegin()) {
+            //     // Dirichlet BC at bottom boundary
+            //     discretization_->p(i, j - 1) = discretization_->p(i, j); // assuming ghost cell already set to BC value
+            // }
+            // if (redCellInfo.leftIsBoundaryFace() && partitioning_->ownPartitionContainsLeftBoundary() && i == discretization_->pIBegin()) {
+            //     // Dirichlet BC at left boundary
+            //     discretization_->p(i - 1, j) = discretization_->p(i, j); // assuming ghost cell already set to BC value
+            // }
+            // if (redCellInfo.rightIsBoundaryFace() && partitioning_->ownPartitionContainsRightBoundary() && i == discretization_->pIEnd()) {
+            //     // Dirichlet BC at right boundary
+            //     discretization_->p(i + 1, j) = discretization_->p(i, j); // assuming ghost cell already set to BC value
+            // }
         }
 
+       
+        // print debug info
         if (iter == 1 && partitioning_->ownRankNo() == 3) {
             std::cout << "After red update in iteration " << iter << ", p field:" << std::endl;
-            for (int j = discretization_->pJEnd(); j <= discretization_->pJBegin(); j--) {
-                for (int i = discretization_->pIBegin(); i <= discretization_->pIEnd(); i++) {
+            for (int j = discretization_->pJEnd()+1; j >= discretization_->pJBegin()-1; j--) {
+                for (int i = discretization_->pIBegin()-1; i <= discretization_->pIEnd()+1; i++) {
                     std::cout << discretization_->p(i, j) << " ";
                 }
                 std::cout << std::endl;
+            }
+        }
+        
+
+         // debug shit
+        const int pIBegin = discretization_->pIBegin();
+        const int pIEnd = discretization_->pIEnd();
+        const int pJBegin = discretization_->pJBegin();
+        const int pJEnd = discretization_->pJEnd();
+        if (partitioning_->ownPartitionContainsTopBoundary()) {
+            for (int i = pIBegin; i <= pIEnd; i++) {
+                discretization_->p(i, pJEnd + 1) = discretization_->p(i, pJEnd);
+            }
+        }
+        if (partitioning_->ownPartitionContainsBottomBoundary()) {
+        for (int i = pIBegin; i <= pIEnd; i++) {
+            discretization_->p(i, pJBegin - 1) = discretization_->p(i, pJBegin);
+        }
+        }
+        if (partitioning_->ownPartitionContainsLeftBoundary()) {
+            for (int j = pJBegin; j <= pJEnd; j++) {
+                discretization_->p(pIBegin - 1, j) = discretization_->p(pIBegin, j);
+            }
+        }
+        if (partitioning_->ownPartitionContainsRightBoundary()) {
+            for (int j = pJBegin; j <= pJEnd; j++) {
+                discretization_->p(pIEnd + 1, j) = discretization_->p(pIEnd, j);
             }
         }
         
@@ -76,8 +123,8 @@ void DomainRBGaussSeidel::solve() {
         communicateGhostValues();
         if (iter == 1 && partitioning_->ownRankNo() == 3) {
             std::cout << "After communicating red cells in iteration " << iter << ", p field:" << std::endl;
-            for (int j = discretization_->pJEnd(); j <= discretization_->pJBegin(); j--) {
-                for (int i = discretization_->pIBegin(); i <= discretization_->pIEnd(); i++) {
+            for (int j = discretization_->pJEnd()+1; j >= discretization_->pJBegin()-1; j--) {
+                for (int i = discretization_->pIBegin()-1; i <= discretization_->pIEnd()+1; i++) {
                     std::cout << discretization_->p(i, j) << " ";
                 }
                 std::cout << std::endl;
@@ -100,35 +147,79 @@ void DomainRBGaussSeidel::solve() {
 
             if (blackCellInfo.topIsBoundaryFace()) {
                 p_i_jp1 = p_i_j;
-                if (partitioning_->ownPartitionContainsTopBoundary() && j == discretization_->pJEnd()) {
-                    // Dirichlet BC at top boundary
-                    discretization_->p(i, j + 1) = p_i_jp1; // assuming ghost cell already set to BC value
-                }
+                // if (partitioning_->ownPartitionContainsTopBoundary() && j == discretization_->pJEnd()) {
+                //     // Dirichlet BC at top boundary
+                //     discretization_->p(i, j + 1) = p_i_jp1; // assuming ghost cell already set to BC value
+                // }
             }
             if (blackCellInfo.bottomIsBoundaryFace()) {
                 p_i_jm1 = p_i_j;
-                if (partitioning_->ownPartitionContainsBottomBoundary() && j == discretization_->pJBegin()) {
-                    // Dirichlet BC at bottom boundary
-                    discretization_->p(i, j - 1) = p_i_jm1; // assuming ghost cell already set to BC value
-                }
+                // if (partitioning_->ownPartitionContainsBottomBoundary() && j == discretization_->pJBegin()) {
+                //     // Dirichlet BC at bottom boundary
+                //     discretization_->p(i, j - 1) = p_i_jm1; // assuming ghost cell already set to BC value
+                // }
             }
             if (blackCellInfo.leftIsBoundaryFace()) {
                 p_im1_j = p_i_j;
-                if (partitioning_->ownPartitionContainsLeftBoundary() && i == discretization_->pIBegin()) {
-                    // Dirichlet BC at left boundary
-                    discretization_->p(i - 1, j) = p_im1_j; // assuming ghost cell already set to BC value
-                }
+                // if (partitioning_->ownPartitionContainsLeftBoundary() && i == discretization_->pIBegin()) {
+                //     // Dirichlet BC at left boundary
+                //     discretization_->p(i - 1, j) = p_im1_j; // assuming ghost cell already set to BC value
+                // }
             }
             if (blackCellInfo.rightIsBoundaryFace()) {
                 p_ip1_j = p_i_j;
-                if (partitioning_->ownPartitionContainsRightBoundary() && i == discretization_->pIEnd()) {
-                    // Dirichlet BC at right boundary
-                    discretization_->p(i + 1, j) = p_ip1_j; // assuming ghost cell already set to BC value
-                }
+                // if (partitioning_->ownPartitionContainsRightBoundary() && i == discretization_->pIEnd()) {
+                //     // Dirichlet BC at right boundary
+                //     discretization_->p(i + 1, j) = p_ip1_j; // assuming ghost cell already set to BC value
+                // }
             }
 
             discretization_->p(i,j) = lek * ((p_ip1_j + p_im1_j)/dx2 + (p_i_jp1 + p_i_jm1) / dy2 - discretization_->rhs(i,j));
+
+            if (blackCellInfo.topIsBoundaryFace() && partitioning_->ownPartitionContainsTopBoundary() && j == discretization_->pJEnd()) {
+                // Dirichlet BC at top boundary
+                discretization_->p(i, j + 1) = discretization_->p(i, j); // assuming ghost cell already set to BC value
+            }
+            if (blackCellInfo.bottomIsBoundaryFace() && partitioning_->ownPartitionContainsBottomBoundary() && j == discretization_->pJBegin()) {
+                // Dirichlet BC at bottom boundary
+                discretization_->p(i, j - 1) = discretization_->p(i, j); // assuming ghost cell already set to BC value
+            }
+            if (blackCellInfo.leftIsBoundaryFace() && partitioning_->ownPartitionContainsLeftBoundary() && i == discretization_->pIBegin()) {
+                // Dirichlet BC at left boundary
+                discretization_->p(i - 1, j) = discretization_->p(i, j); // assuming ghost cell already set to BC value
+            }
+            if (blackCellInfo.rightIsBoundaryFace() && partitioning_->ownPartitionContainsRightBoundary() && i == discretization_->pIEnd()) {
+                // Dirichlet BC at right boundary
+                discretization_->p(i + 1, j) = discretization_->p(i, j); // assuming ghost cell already set to BC value
+            }
         }
+
+        // set boundary values before communicating black cells
+        const int pIBegin = discretization_->pIBegin();
+        const int pIEnd = discretization_->pIEnd();
+        const int pJBegin = discretization_->pJBegin();
+        const int pJEnd = discretization_->pJEnd();
+        if (partitioning_->ownPartitionContainsTopBoundary()) {
+            for (int i = pIBegin; i <= pIEnd; i++) {
+                discretization_->p(i, pJEnd + 1) = discretization_->p(i, pJEnd);
+            }
+        }
+        if (partitioning_->ownPartitionContainsBottomBoundary()) {
+        for (int i = pIBegin; i <= pIEnd; i++) {
+            discretization_->p(i, pJBegin - 1) = discretization_->p(i, pJBegin);
+        }
+        }
+        if (partitioning_->ownPartitionContainsLeftBoundary()) {
+            for (int j = pJBegin; j <= pJEnd; j++) {
+                discretization_->p(pIBegin - 1, j) = discretization_->p(pIBegin, j);
+            }
+        }
+        if (partitioning_->ownPartitionContainsRightBoundary()) {
+            for (int j = pJBegin; j <= pJEnd; j++) {
+                discretization_->p(pIEnd + 1, j) = discretization_->p(pIEnd, j);
+            }
+        }
+
         
         // Communicate black cell values and set boundary values
         communicateGhostValues();
